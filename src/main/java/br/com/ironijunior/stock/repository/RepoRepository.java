c356185a3540cde11a954c64156d7ff79b40c5d2
Ironi Jr. Medina <ironimedina@gmail.com>
2019-05-19 23:01:32 -0300

    Remove unnecessary files

--- src/main/java/br/com/ironijunior/stock/repository/RepoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ironijunior.stock.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.ironijunior.stock.model.Repo;
-
-public interface RepoRepository extends JpaRepository<Repo, Long> {
-}
